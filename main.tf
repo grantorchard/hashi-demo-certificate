f7b56e4729968cce964437f555547101349c8af9
grantorchard <go@hashicorp.com>
2019-12-03 17:48:53 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,32 @@
 resource aws_acm_certificate "vault" {
+  domain_name       = "vault.go.hashidemos.io"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource aws_acm_certificate "tfe" {
   domain_name       = "tfe.go.hashidemos.io"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource aws_acm_certificate "consul" {
+  domain_name       = "tfe.go.hashidemos.io"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource aws_acm_certificate "wildcard" {
+  domain_name       = "*.go.hashidemos.io"
   validation_method = "DNS"
 
   lifecycle {
